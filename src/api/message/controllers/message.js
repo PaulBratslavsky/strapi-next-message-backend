50173f60823a6195dd16d074ce537df3332be204
Paul Bratslavsky <paulbratslavsky@Pauls-MacBook-Pro.local>
2022-05-11 14:00:18 -0400

    added simple polling via useInterval hool

--- src/api/message/controllers/message.js
+++ src/api/message/controllers/message.js
@@ -1,4 +1,5 @@
 "use strict";
+const { nanoid } = require("nanoid");
 
 /**
  *  message controller
@@ -7,29 +8,54 @@
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::message.message", ({ strapi }) => ({
-    async find(ctx) {
-        // 1
-        const entries = await strapi.entityService.findMany(
-          'api::message.message',
-          {
-            sort: { createdAt: 'DESC' },
-            limit: 5,
-          }
-        );
-      
-        console.log(this, "################## this ######################");
-        // 2
-        const sanitizedEntries = await this.sanitizeOutput(entries, ctx);
-      
-        // 3
-        return this.transformResponse(sanitizedEntries);
-      },
+  async find(ctx) {
+    const entries = await strapi.entityService.findMany(
+      "api::message.message",
+      {
+        sort: { createdAt: "DESC" },
+        limit: 5,
+      }
+    );
+
+    const sanitizedEntries = await this.sanitizeOutput(entries, ctx);
+    return this.transformResponse(sanitizedEntries);
+  },
 
   async create(ctx) {
-    // todo
+    ctx.request.body.data = {
+      ...ctx.request.body.data,
+      uid: nanoid(),
+      timesUpdated: 0,
+    };
+
+    // extends existing create controller
+    const response = await super.create(ctx);
+    return response;
   },
 
   async update(ctx) {
-    // todo
+    const { id } = ctx.params;
+
+    // find message based on id
+    const entry = await strapi.entityService.findOne(
+      "api::message.message",
+      id
+    );
+
+    // delete data from request body
+    delete ctx.request.body.data.uid;
+    delete ctx.request.body.data.postedBy;
+
+    // update the request body data
+    ctx.request.body.data = {
+      ...ctx.request.body.data,
+      timesUpdated: entry.timesUpdated + 1,
+    };
+
+    // NOTE: you can update request body data directly and it is saved in ctx.
+
+    // extended from existing update controller
+    const response = await super.update(ctx);
+    return response;
   },
 }));
